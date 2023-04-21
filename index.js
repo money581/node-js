7b94e618be95045b5d19c0d587172ee6c81a3707
Aman <amanagrawal581@gmail.com>
2023-04-21 21:50:36 +0530

    1st crete srvr nd cnsole name by calling this srvr

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world! from node js")
+//console.log("hello world! from node js")
+const http=require('http');
+// function rqListener(req,res){
+
+// }
+//http.createServer(rqListener);
+//we can also write this by anonymus function
+// http.createServer(function(req,res){
+
+// })
+//we can also write as arrow function
+ const server = http.createServer((req , res) => {
+console.log('aman');
+ });
+ server.listen(4000);
