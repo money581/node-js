7808be5fecf2ab81b7480d007331dfe96b781e7a
Aman <amanagrawal581@gmail.com>
2023-04-23 23:25:20 +0530

    12th commit lets get started with express js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 //console.log("hello world! from node js")
 const http = require('http');
+const express=require('express');
+const app=express();
 
-
-const { buffer } = require('stream/consumers');
-console.log(routes.someText)
-const server = http.createServer();
-server.listen(4000);
+app.use((req,res,next)=>{
+    console.log('in the middleware')
+    next();  //aloow to request to continue to the next middleware
+});
+app.use((req,res,next)=>{
+    console.log('another middleware');
+    res.send('<h1>hello from express js!</h1>');
+    
+});
+app.listen(3000);
