3a46896537ebb762ef24c451efb1e2ded2416ea9
Aman <amanagrawal581@gmail.com>
2023-04-25 21:45:16 +0530

    in express js, intro to middleware

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-//console.log("hello world! from node js")
-const http = require('http');
-const express=require('express');
-const app=express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-app.use((req,res,next)=>{
-    console.log('in the middleware')
-    next();  //aloow to request to continue to the next middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/add-product', (req, res, next) => {
+
+    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="add product name"><input type="text" name="size" placeholder="size of product"><button type="submit">Add Product</button><form>');
 });
-app.use((req,res,next)=>{
-    console.log('another middleware');
-    res.send('<h1>hello from express js!</h1>');
-    
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+});
+
+app.use((req, res, next) => {
+
+    res.send('<h1>hello from express jss!</h1>');
+
 });
 app.listen(3000);
