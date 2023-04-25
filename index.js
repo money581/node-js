a6f2ade644731aba29f738ab0a79148ec1ac95c7
Aman <amanagrawal581@gmail.com>
2023-04-25 23:12:53 +0530

    in express.js  routers and filters

--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const adminRoutes=require('./routes/admin');
+const shopRoutes=require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/add-product', (req, res, next) => {
-
-    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="add product name"><input type="text" name="size" placeholder="size of product"><button type="submit">Add Product</button><form>');
-});
-
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/')
-});
-
-app.use((req, res, next) => {
-
-    res.send('<h1>hello from express jss!</h1>');
-
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>page not found</h1>')
 });
 app.listen(3000);
