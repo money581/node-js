7f53cba0edf6aada923e655e53024a5f31335960
Aman <amanagrawal581@gmail.com>
2023-05-03 22:35:58 +0530

    Srving HTML , CSS and JS (VIEWS) tsk no 5 exprs.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
+const path=require('path');
 const express=require('express');
+const rootDir=require('../util/path');
 const router=express.Router();
 router.get('/add-product', (req, res, next) => {
 
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" placeholder="add product name"><input type="text" name="size" placeholder="size of product"><button type="submit">Add Product</button><form>');
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
@@ -10,3 +12,4 @@ router.post('/add-product', (req, res, next) => {
     res.redirect('/')
 });
 module.exports=router;
+
--- routes/success.js
+++ routes/success.js
@@ -2,9 +2,9 @@ const path=require('path');
 const express=require('express');
 const rootDir=require('../util/path');
 const router=express.Router();
-router.get('/',(req, res, next) => {
+router.get('/success',(req, res, next) => {
 
-    res.sendFile(path.join(rootDir,'views','shop.html'));
+    res.sendFile(path.join(rootDir,'views','success.html'));
 
 });
 module.exports=router;
